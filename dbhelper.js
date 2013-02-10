23d8875dcfbaa216b9da8b0b2e7bcc1cf958831f
Smit Shah <who828@gmail.com>
2013-02-10 12:20:54 +0530

    Removed global variables and added semicolons to dbhelper.js

--- dbhelper.js
+++ dbhelper.js
@@ -4,38 +4,38 @@ var usersCollection = 'users';
 var db = require('mongojs').connect(mongourl, Array(usersCollection));
 
 exports.addRecord = function(user, collection, callback) {
-  objectToInsert = { name: user.name, email: user.email,
+  var objectToInsert = { name: user.name, email: user.email,
                       usn: user.usn };
   db.users.insert( objectToInsert , function(err) {
     if (err) { if (typeof callback == "function") callback(err); return; }
   });
-}
+};
 
 exports.removeRecord = function(user, collection, callback) {
-  objectToRemove = { name: user.name, email: user.email,
+  var objectToRemove = { name: user.name, email: user.email,
                       usn: user.usn };
   db.users.remove( objectToRemove , function(err) {
     if (err) { if (typeof callback == "function") callback(err); return; }
   });
-}
+};
 
 exports.listRecords = function(collection, callback) {
   db.users.find().toArray(function(err, results) {
     callback(err, results);
   });
-}
+};
 
 exports.listUsers = function(callback) {
   exports.listRecords(usersCollection, callback);
-}
+};
 
 exports.addUser = function(user, callback) {
   exports.addRecord(user, usersCollection, callback);
-}
+};
 
 exports.removeUser = function(user, callback) {
   exports.removeRecord(user, usersCollection, callback);
-}
+};
 
 exports.disableUser = function(user, callback) {
   exports.addRecord(user, usersCollection + '_trash', function(err) {
@@ -44,4 +44,4 @@ exports.disableUser = function(user, callback) {
   exports.removeUser(user, function(err) {
     if (err) { if (typeof callback == "function") callback(err); return; }
   });
-}
+};
